7f2c3827fd1fbc8a8736914a1c270b1e7647cbbb
conor-mcgowan <conor.mcgowan@live.com>
2021-02-18 16:45:55 -0600

    adds app.js structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <>
+        <Router>
+          <header>
+            <nav className="nav-bar">
+              <NavLink
+                to="/login"
+                className="nav-link text-center"
+                activeClassName="active-link"
+              >
+                Login
+              </NavLink>
+              <NavLink
+                to="/search"
+                className="nav-link text-center"
+                activeClassName="active-link"
+              >
+                Search
+              </NavLink>
+              <NavLink
+                to="/favorites"
+                className="nav-link text-center"
+                activeClassName="active-link"
+              >
+                Favorites
+              </NavLink>
+            </nav>
+          </header>
+          <main>
+            <Switch>
+              <Route exact path="/login" component={LoginPage} />
+              <Route exact path="/search" component={SearchPage} />
+              <Route exact path="/favorites" component={FavoritesPage} />
+              <Route path="*">
+                <Redirect to="/search" />
+              </Route>
+            </Switch>
+          </main>
+          <br />
+          <footer>This is a footer</footer>
+        </Router>
+      </>
+    </Provider>
   );
 }
 
