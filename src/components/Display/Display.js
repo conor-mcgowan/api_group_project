ac0e94989e06d22a94c10e79f948ab1ef4f2d8e4
Mohammad <Your janpishtaz4@gmail.com>
2021-02-19 11:48:24 -0600

    imprt display to search components

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,11 +1,30 @@
 import React from 'react'
 
-const Display = () => {
+const Display = ({name, image, symbol, price, volume, priceChange, marketCap}) => {
     return (
         <div>
-            
+            <div>
+                <div>
+                    <img src={image} alt="crypto"/>
+                    <h1>{name}</h1>
+                    <p>{symbol}</p>
+                </div>
+                <div>
+                    <p>${price}</p>
+                    <p>${volume}</p>
+                    {priceChange < 0 ? (
+                        <p className="coin-percent red">{priceChange.
+                        toFixed(2)}%</p>
+                    ) :(<p className="coin-percent green ">{priceChange.
+                        toFixed(2)}%</p>) 
+                }
+                 <p className="coin-marketCap">
+                     Mkt Cap: ${marketCap}
+                 </p>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Display
+export default Display;
