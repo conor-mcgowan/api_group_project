ed3695a184220135686904c227b2239464ceefa3
conor-mcgowan <conor.mcgowan@live.com>
2021-02-19 12:03:49 -0600

    adds Favorites Page

---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Display from "../Display/Display";
+import { connect } from "react-redux";
+import { deleteFavorite } from "../../redux/actions";
+
+const Favorites = (props) => {
+  <>
+    <h1>FavoritesPage</h1>
+    <div>(Favorites results)</div>
+  </>;
+};
